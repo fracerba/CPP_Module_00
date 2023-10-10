87f3083510de54e4b605e9e7e36acb63894043a8
Francesco Cerbai <129869375+fracerba@users.noreply.github.com>
2023-10-10 16:53:03 +0200

    first commit

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	int i = 0;
+	int j = 0;
+
+	if(argc < 2)
+		std::cout<<"* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+	else
+	{
+		while (argv[++i])
+		{
+			j = 0;
+			while (argv[i][j])
+			{
+				if (argv[i][j] >= 'a' && argv[i][j] <= 'z')
+					std::cout<<(char)(argv[i][j] - 32);
+            	else
+					std::cout<<argv[i][j];
+				j++;
+			}
+		}
+		std::cout<<"\n";
+	}
+	return(0);
+}
